9ef54200c711dcd942261fe2d06060b00ac0d266
JoungEunLee <amy1123kr@gmail.com>
2018-06-24 01:47:20 +0900

    네이버 지도 api

--- app/src/main/java/com/example/amya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/amya/myapplication/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.amya.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.os.Bundle;
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NMapActivity {
+
+    private ViewGroup mapLayout;
+    private NMapView mMapView;// 지도 화면 View
+    private final String CLIENT_ID = "LDSmLMPEx7hDpJKRyxae";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        TextView textView2 = (TextView)findViewById( R.id.textView2 );
-        textView2.setText( "Hello World!!!!!" );
+
+        mMapView = new NMapView(this);
+        setContentView(mMapView);
+        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
 
     }
 
